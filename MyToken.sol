81860816470747ffc63e49577a353c6b4166de32
Dozgor <119602202+Dozgor@users.noreply.github.com>
2025-05-28 07:49:00 +0200

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// This contract creates a basic ERC20 token using OpenZeppelin's implementation.
 contract MyToken is ERC20 {
+    // The constructor sets the token name and symbol, and mints 1000 tokens to the deployer.
     constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
